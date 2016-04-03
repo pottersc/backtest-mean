6cef5c567e38017358173ebb88683aa39de47416
Scott Potter <scott.g.potter@gmail.com>
2016-04-03 11:49:27 -0400

    cleaned up code & added documentation

--- client/app/services/historicalQuotes/historicalQuotes.service.js
+++ client/app/services/historicalQuotes/historicalQuotes.service.js
@@ -9,6 +9,11 @@ angular.module('backtestMeanApp')
 
     /**
      * Retrieve the historical stock quotes for the specified ticker symbol and dates
+     * This approach uses a REST service on the server to pass thru the quotes because
+     * the Yahoo API does not support direct access from a client (does not support
+     * Cross Origin Resource Sharing).
+     * The other function 'getQuotesDirect' is a preferred access mehtod, but only works
+     * if a CORS plugin is installed on the client which is an unacceptable security risk
      * @param ticker : ticker symbol of the stock
      * @param start : first date to collect data for
      * @param end : last date to collect data for
@@ -26,43 +31,21 @@ angular.module('backtestMeanApp')
             quotes.push(quote);
 
           });
-          console.log(quotes);
           if(!quotes){
             reject('error accessing quotes');
           }else {
             resolve(quotes);
           }
         });
-
-
-        //$http.get(url)
-        //  .success(function (csvContents) {
-        //    var first = true;
-        //    var quotes = [];
-        //    _.forEach(_.split(csvContents,'\n'),function(line) {
-        //      // line format is: Date,Open,High,Low,Close,Volume,Adj Close
-        //      if (!first && line.length > 10) {
-        //        var fields = _.split(line, ',');
-        //        var quote = {date: parseDate(fields[0]), price: Number(_.trim(fields[6]))};
-        //        quotes.push(quote);
-        //      }
-        //      first = false;
-        //    });
-        //    _.reverse(quotes);
-        //    resolve(quotes);
-        //  })
-        //  .error(function (err) {
-        //    reject(err);
-        //  });
       });
     };
 
-
-
-
-
     /**
      * Retrieve the historical stock quotes for the specified ticker symbol and dates
+     * This approach is not acceptable in production because it only runs with a CORS
+     * plugin installed on the client which is a security risk. In the future if Yahoo or
+     * another site supports accessible REST service for historical quotes then this approach
+     * should be considered again.  For now this function should not be used in production.
      * @param ticker : ticker symbol of the stock
      * @param start : first date to collect data for
      * @param end : last date to collect data for
@@ -107,6 +90,14 @@ angular.module('backtestMeanApp')
       return new Date(Date.UTC(year,month,day,5,0,0));
     }
 
+    /**
+     * A useful utility function for generating fake quotes that is useful for testing and debugging.
+     * It returns a repeating triangle wave of stock prices that oscillate between 90 and 110
+     * @param ticker : ticker symbol of the stock
+     * @param start : first date to collect data for
+     * @param end : last date to collect data for
+     * @returns {Promise} : success returns an array of stock quotes
+       */
     HistoricalQuotesService.getFakeQuotes = function(ticker, start, end){
       return new Promise(function(resolve, reject) {
         var quotes = [];
@@ -136,7 +127,6 @@ angular.module('backtestMeanApp')
         resolve(quotes);
       });
     };
-
 
     return HistoricalQuotesService;
   }]);
